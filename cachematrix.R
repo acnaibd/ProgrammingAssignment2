760e1897fbd7bdfde36717bc17771d07e101a5f9
Bianca Drumea <acnaib.d@gmail.com>
2016-09-22 13:31:39 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,6 @@
 ## This function contains internal storage fit for a single matrix, calculated in below function
 ## It does not compute 
 ## It contains functions used to get/set a regular matrix and get/set an inverted matrix
-
-
 makeCacheMatrix <- function(x = matrix()) {
   inv = NULL
     set <- function(y) {
@@ -20,7 +18,6 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## This function contains functionality used to manage an internal cache for a single matrix
 ## Function takes a regular matrix with assigned parameters and creates an improved, invertible version of the matrix 
-
 cacheSolve <- function(x, ...) {
   m <- x$getInverse()
   if(!is.null(m)) {
