d7d3c81e598add0cbb5471026e2e5e61a7d1b2d2
Bianca Drumea <acnaib.d@gmail.com>
2016-09-22 13:30:51 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function contains internal storage fit for a single matrix, calculated in below function
+## It does not compute 
+## It contains functions used to get/set a regular matrix and get/set an inverted matrix
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv = NULL
+    set <- function(y) {
+      x <<- y
+      inv <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) inv <<- inverse
+    getInverse <- function() inv
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function contains functionality used to manage an internal cache for a single matrix
+## Function takes a regular matrix with assigned parameters and creates an improved, invertible version of the matrix 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  message("nothing cached. calculating")
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverse(m)
+  m
 }
